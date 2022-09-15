1dd4ac2ef15aefad4b99776f4ddf6ec8c8da1bc2
LoTwT <709937065@qq.com>
2022-09-15 10:41:58 +0800

    feat: Making Our Block Editable

--- our-blocks/banner.js
+++ our-blocks/banner.js
@@ -1,3 +1,5 @@
+import { InnerBlocks } from "@wordpress/block-editor"
+
 wp.blocks.registerBlockType("ourblocktheme/banner", {
   title: "Banner",
   edit: EditComponent,
@@ -5,29 +7,55 @@ wp.blocks.registerBlockType("ourblocktheme/banner", {
 })
 
 function EditComponent() {
+  const useMeLater = (
+    <>
+      <h1 className="headline headline--large">Welcome!</h1>
+      <h2 className="headline headline--medium">
+        We think you&rsquo;ll like it here.
+      </h2>
+      <h3 className="headline headline--small">
+        Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re
+        interested in?
+      </h3>
+      <a href="#" className="btn btn--large btn--blue">
+        Find Your Major
+      </a>
+    </>
+  )
+
   return (
     <div className="page-banner">
       <div
         className="page-banner__bg-image"
-        style={{ backgroundImage: "url('/wordpress/wp-content/themes/fictional-block-theme/images/library-hero.jpg')" }}
+        style={{
+          backgroundImage:
+            "url('/wordpress/wp-content/themes/fictional-block-theme/images/library-hero.jpg')",
+        }}
       />
       <div className="page-banner__content container t-center c-white">
-        <h1 className="headline headline--large">Welcome!</h1>
-        <h2 className="headline headline--medium">
-          We think you&rsquo;ll like it here.
-        </h2>
-        <h3 className="headline headline--small">
-          Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re
-          interested in?
-        </h3>
-        <a href="#" className="btn btn--large btn--blue">
-          Find Your Major
-        </a>
+        {/* 内部块，提供编辑功能 */}
+        <InnerBlocks
+          allowedBlocks={["core/paragraph", "core/heading", "core/list"]}
+        />
       </div>
     </div>
   )
 }
 
 function SaveComponent() {
-  return <p>This is from our block.</p>
+  return (
+    <div className="page-banner">
+      <div
+        className="page-banner__bg-image"
+        style={{
+          backgroundImage:
+            "url('/wordpress/wp-content/themes/fictional-block-theme/images/library-hero.jpg')",
+        }}
+      />
+      <div className="page-banner__content container t-center c-white">
+        {/* 内部块的输出，保存到数据库的内容 */}
+        <InnerBlocks.Content />
+      </div>
+    </div>
+  )
 }
