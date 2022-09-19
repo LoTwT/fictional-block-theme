8fede62d1cfb91b191cbf29965b07796e685e322
LoTwT <709937065@qq.com>
2022-09-19 11:14:13 +0800

    feat: User Uploaded Background Images

--- our-blocks/banner.js
+++ our-blocks/banner.js
@@ -1,5 +1,13 @@
-import { InnerBlocks } from "@wordpress/block-editor"
+import {
+  InnerBlocks,
+  InspectorControls,
+  MediaUpload,
+  MediaUploadCheck,
+} from "@wordpress/block-editor"
+import { Button, PanelBody, PanelRow } from "@wordpress/components"
 import { registerBlockType } from "@wordpress/blocks"
+import apiFetch from "@wordpress/api-fetch"
+import { useEffect } from "@wordpress/element"
 
 registerBlockType("ourblocktheme/banner", {
   title: "Banner",
@@ -8,47 +16,74 @@ registerBlockType("ourblocktheme/banner", {
   },
   attributes: {
     align: { type: "string", default: "full" },
+    imgID: { type: "number" },
+    imgURL: { type: "string" },
   },
   edit: EditComponent,
   save: SaveComponent,
 })
 
-function EditComponent() {
-  // const useMeLater = (
-  //   <>
-  //     <h1 className="headline headline--large">Welcome!</h1>
-  //     <h2 className="headline headline--medium">
-  //       We think you&rsquo;ll like it here.
-  //     </h2>
-  //     <h3 className="headline headline--small">
-  //       Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re
-  //       interested in?
-  //     </h3>
-  //     <a href="#" className="btn btn--large btn--blue">
-  //       Find Your Major
-  //     </a>
-  //   </>
-  // )
+function EditComponent(props) {
+  useEffect(() => {
+    const go = async () => {
+      const response = await apiFetch({
+        method: "GET",
+        path: `/wp/v2/media/${props.attributes.imgID}`,
+      })
+      props.setAttributes({
+        imgURL: response.media_details.sizes.pageBanner.source_url,
+      })
+    }
+
+    go()
+  }, [props.attributes.imgID])
+
+  const onFileSelect = (img) => {
+    props.setAttributes({ imgID: img.id })
+  }
 
   return (
-    <div className="page-banner">
-      <div
-        className="page-banner__bg-image"
-        style={{
-          backgroundImage:
-            "url('/wordpress/wp-content/themes/fictional-block-theme/images/library-hero.jpg')",
-        }}
-      />
-      <div className="page-banner__content container t-center c-white">
-        {/* 内部块，提供编辑功能 */}
-        <InnerBlocks allowedBlocks={["ourblocktheme/genericheading", "ourblocktheme/genericbutton"]} />
+    <>
+      {/* 侧边菜单上传 */}
+      <InspectorControls>
+        <PanelBody title="Background" initialOpen>
+          <PanelRow>
+            <MediaUploadCheck>
+              <MediaUpload
+                onSelect={onFileSelect}
+                value={props.attributes.imgID}
+                // 实际渲染在侧边菜单中的组件样式，wordpress 内置了打开媒体上传对话框的方法 open
+                render={({ open }) => (
+                  <Button onClick={open}>Choose Image</Button>
+                )}
+              />
+            </MediaUploadCheck>
+          </PanelRow>
+        </PanelBody>
+      </InspectorControls>
+
+      {/* 页面 */}
+      <div className="page-banner">
+        <div
+          className="page-banner__bg-image"
+          style={{
+            backgroundImage: `url("${props.attributes.imgURL}")`,
+          }}
+        />
+        <div className="page-banner__content container t-center c-white">
+          {/* 内部块，提供编辑功能 */}
+          <InnerBlocks
+            allowedBlocks={[
+              "ourblocktheme/genericheading",
+              "ourblocktheme/genericbutton",
+            ]}
+          />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
 function SaveComponent() {
-  return (
-    <InnerBlocks.Content />
-  )
+  return <InnerBlocks.Content />
 }
--- our-blocks/banner.php
+++ our-blocks/banner.php
@@ -1,5 +1,14 @@
+<?php
+
+  // 容错
+  if (!$attributes["imgURL"]) {
+    $attributes["imgURL"] = get_theme_file_uri('/images/library-hero.jpg');
+  }
+
+?>
+
 <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url('<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>')"></div>
+  <div class="page-banner__bg-image" style="background-image: url('<?php echo $attributes["imgURL"]; ?>')"></div>
   <div class="page-banner__content container t-center c-white">
     <?php echo $content; ?>
   </div>
