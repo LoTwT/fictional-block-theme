2ddc2b3e893a7e625cf2d81776eda10947621bf8
LoTwT <709937065@qq.com>
2022-09-22 17:54:15 +0800

    fix: php parse error

--- our-blocks/eventsandblogs.php
+++ our-blocks/eventsandblogs.php
@@ -27,11 +27,18 @@
 
           <div class="event-summary">
             <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-              <span class="event-summary__month"><?php 
-                $eventDate = new DateTime(get_field("event_date"));
-                echo $eventDate->format("M");
-              ?></span>
-              <span class="event-summary__day"><?php echo $eventDate->format("d"); ?></span>
+              <span class="event-summary__month">
+                <!-- 导致 Doctype 解析错误，注释 -->
+                <?php 
+                  // $eventDate = new DateTime(get_field("event_date"));
+                  // echo $eventDate->format("M");
+                ?>
+              </span>
+              <span class="event-summary__day">
+                <?php 
+                  // echo $eventDate->format("d"); 
+                ?>
+              </span>
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
